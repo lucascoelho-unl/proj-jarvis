0b902483dc0c338cd33f5a11a9eaee33d7813a80
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-04-16 00:46:09 -0500

    commit	new file:   .DS_Store commit	modified:   src/audio_input.py commit	modified:   voice_output.mp3

--- src/audio_input.py
+++ src/audio_input.py
@@ -5,16 +5,17 @@ recognizer.energy_threshold = 10000
 
 def capture_voice_input():
     with sr.Microphone() as source:
+        print("\n> ", end='')
         audio = recognizer.listen(source)
     return audio
 
 def convert_voice_to_text(audio):
     try:
         text = recognizer.recognize_google(audio)
-        print("\n> " + text)
+        print(text)
     except sr.UnknownValueError:
         text = ""
-        print("Sorry, I didn't understand that.")
+        print("\n> Not understandable audio input. Try again")
     except sr.RequestError as e:
         text = ""
         print("Error; {0}".format(e))
